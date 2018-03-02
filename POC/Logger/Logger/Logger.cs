bda3a53ba657ad392cc2c04dc67939f52279eb76
Linson Augustine <LinsonA@emids.com>
2018-03-02 11:22:14 +0530

    first commit

---
+++ POC/Logger/Logger/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace Logger
+{
+    public class Log
+    {
+        private readonly string filePath;
+
+        public Log(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public void Transaction (string message)
+        {
+            this.LogProcess("Transaction", message);
+        }
+
+        public void Exception (string message)
+        {
+            this.LogProcess("Exception", message);
+        }
+
+        public void Exception (string message, string stackTrace)
+        {
+            this.LogProcess("Exception", message, stackTrace);
+        }
+
+        private void LogProcess(string fileName, string message1, string message2 = "")
+        {
+            string combinedFilePath = Path.Combine(this.filePath, string.Format("{0}_{1}.log", fileName, DateTime.Now.ToString("MM-dd-yyyy")));
+            
+            if (File.Exists(combinedFilePath))
+            {
+                using (StreamWriter writer = File.AppendText(combinedFilePath))
+                {
+                   WriteLine(writer, message1, message2);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.CreateText((combinedFilePath)))
+                {
+                   WriteLine(writer, message1, message2);
+                }
+            }
+        }
+
+        private async void WriteLine(StreamWriter writer, string message1, string message2 = "")
+        {
+            await writer.WriteLineAsync(message1);
+
+            if(!string.IsNullOrEmpty(message2))
+            {
+                await writer.WriteLineAsync(message2);
+            }
+        }
+    }
+}
---
+++ POC/Logger/LoggerApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Logger;
+
+namespace LoggerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"..\Output\";
+            Log log = new Log(filePath);
+            log.Exception("First Exception Logged");
+            log.Exception("Second Exception Logged");
+            Console.WriteLine("File has created for Exception");
+
+            log.Transaction("First Transaction Logged");
+            log.Transaction("Second Transaction Logged");
+            Console.WriteLine("File has created for Transaction");
+
+            try
+            {
+                throw new FieldAccessException();
+            }
+            catch (Exception e)
+            {
+                log.Exception(e.Message);
+                log.Exception(e.Message, e.StackTrace);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
